8765458217054793c6bd90bd29aa9c45cea1b0d4
Promise Nmor <nmorchibogu98@gmail.com>
2024-01-23 21:55:11 +0100

    added bootstrap to the page

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Promise',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 23, 2024'
+    },
+    {
+
+        'author': 'Rocky',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 24, 2024'
+
+
+    }
+
+]
+
 
 def home(requests):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(requests, 'blog/home.html', context)
 
+def about(requests):
+    return render(requests, 'blog/about.html', {'title': 'About'}) 
