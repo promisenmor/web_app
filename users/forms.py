b0759e9c79aa08cc7bc4692513c8bf1a40881d08
Promise Nmor <nmorchibogu98@gmail.com>
2024-02-10 16:02:53 +0100

    creted profile update form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import profile
 
 class UseRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,4 +9,18 @@ class UseRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = profile
+        fields = ['image']
 
