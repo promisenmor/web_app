9b9127feee8cf7657aa351da74d524648344b1e2
Promise Nmor <nmorchibogu98@gmail.com>
2024-02-16 11:43:10 +0100

    created password reset form

--- mouthpiece/settings.py
+++ mouthpiece/settings.py
@@ -133,6 +133,13 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nmorchibogu98@gmail.com'
+EMAIL_HOST_PASSWORD = 'ubnb hmyb rboa ipng'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
